be1e0f16e6a340159a7021d820e2d0b637cb101a
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2017-06-20 21:09:16 -0300

    enhancing coding style

--- drivers/char/optimsoc-noc.c
+++ drivers/char/optimsoc-noc.c
@@ -1,6 +1,12 @@
 /*
- *  chardev.c: Creates a read-only char device that says how many times
- *  you've read from the dev file
+ * OpTiMSoC NoC Driver - https://www.optimsoc.org
+ *
+ * Copyright(C) 2017 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
  */
 
 #include <asm/io.h>
@@ -9,65 +15,103 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
-#define OPTIMSOC_NA_HWADDR 0xe0100000 
-
-#define DEVICE_NAME "optimsoc"
-
+/*
+ * Success.
+ */
 #define SUCCESS 0
 
-static int Major;
-static int Device_Open = 0;
-static int *w, *r;
+/*
+ * Device name.
+ */
+#define DEVICE_NAME "optimsoc-noc"
 
-irqreturn_t irq_handler(int irq, void *foobar)
+/*
+ * Base hardware address for NoC adapter.
+ */
+#define OPTIMSOC_NOC_HWADDR 0xe0100000 
+
+/*
+ * NoC adapter memory size.
+ */
+#define OPTIMSOC_NOC_MEM_SIZE 4096
+
+/*
+ * NoC adapter IRQ number.
+ */
+#define OPTIMSOC_NOC_IRQ 5
+
+/*
+ * Major device number.
+ */
+static int major;
+
+/**
+ * Read pointer.
+ */
+static int *rptr = NULL;
+
+/**
+ * Write pointer.
+ */
+static int *wptr = NULL;
+
+/*
+ * Number of threads using the device. 
+ */
+static int nopen = 0;
+
+/*
+ * Handles a NoC IRQ.
+ */
+static irqreturn_t irq_handler(int irq, void *opaque)
 {
 	printk(KERN_INFO "interrupt fired\n");
 
 	return (IRQ_HANDLED);
 }
 
-/* 
- * Called when a process tries to open the device file, like
- * "cat /dev/mycharfile"
+/*
+ * Opens the NoC device.
  */
 static int device_open(struct inode *inode, struct file *file)
 {
-	if (Device_Open > 0)
+	/* Device already in use. */
+	if (nopen > 0)
 		return (-EBUSY);
 
-	w = r = ioremap_nocache(OPTIMSOC_NA_HWADDR, 4096);
+	wptr = rptr = 
+		ioremap_nocache(OPTIMSOC_NOC_HWADDR, OPTIMSOC_NOC_MEM_SIZE);
 
-	Device_Open++;
+	nopen++;
 	try_module_get(THIS_MODULE);
 
 	return (SUCCESS);
 }
 
-/* 
- * Called when a process closes the device file.
+/*
+ * Closes the NoC device.
  */
 static int device_release(struct inode *inode, struct file *file)
 {
-	Device_Open--;
+	nopen--;
 	module_put(THIS_MODULE);
 
 	return (SUCCESS);
 }
 
-/* 
- * Called when a process, which already opened the dev file, attempts to
- * read from it.
+/*
+ * Reads bytes from the NoC device.
  */
 static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t * offset)
 {
-	w += 512;
-	*w = *r;
+	wptr += 512;
+	*wptr = *rptr;
 
 	return (0);
 }
 
-/*  
- * Called when a process writes to dev file: echo "hi" > /dev/hello 
+/*
+ * Writes butes from the NoC device.
  */
 static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
@@ -83,42 +127,42 @@ static struct file_operations fops = {
 
 
 /*
- * This function is called when the module is loaded
+ * Initializes the device driver module.
  */
-static int __init hello_init_module(void)
+static int __init optimsoc_module_init(void)
 {
 	int ret;
 
-	Major = register_chrdev(0, DEVICE_NAME, &fops);
+	/* Register interrupt device driver. */
+	major = register_chrdev(0, DEVICE_NAME, &fops);
+	if (major < 0)
+	{
+		printk(KERN_ALERT "%s: failed to register device", DEVICE_NAME);
 
-	if (Major < 0) {
-	  printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-	  return Major;
+		return (major);
 	}
 
-	ret = request_irq(5, irq_handler, IRQF_SHARED | IRQF_TRIGGER_HIGH, "optimsoc-na-handler", (void *)(irq_handler));
-
-	/* Failed to register interrupt handler. */
+	/* Register interrupt handler. */
+	ret = request_irq(OPTIMSOC_NOC_IRQ, irq_handler,IRQF_SHARED | IRQF_TRIGGER_HIGH, "optimsoc-noc-handler", (void *)(irq_handler));
 	if (ret != SUCCESS)
 		return (ret);
 
-	printk(KERN_INFO "%s: major number %d", DEVICE_NAME, Major);
-	printk(KERN_INFO "%s: invoke 'mknod /dev/%s c %d 0' to instantiate this device", DEVICE_NAME, DEVICE_NAME, Major);
+	printk(KERN_INFO "%s: major number %d", DEVICE_NAME, major);
+	printk(KERN_INFO "%s: invoke 'mknod /dev/%s c %d 0' to instantiate this device", DEVICE_NAME, DEVICE_NAME, major);
 
 	return (ret);
 }
 
 /*
- * This function is called when the module is unloaded
+ * Unloads the device driver module.
  */
-static void __exit hello_cleanup_module(void)
+static void __exit optimsoc_module_cleanup(void)
 {
-	unregister_chrdev(Major, DEVICE_NAME);
+	unregister_chrdev(major, DEVICE_NAME);
 }
 
-module_init(hello_init_module);
-module_exit(hello_cleanup_module);
-
+module_init(optimsoc_module_init);
+module_exit(optimsoc_module_cleanup);
 
 MODULE_INFO(intree, "Y");
 MODULE_LICENSE("GPL");
